7d63f0097e681d2d22a05e08d5382389078fc27d
Ryan Hansen <rphansen91@gmail.com>
2018-04-21 13:50:01 -0700

    Create conference room

--- src/connectors/twilioConnector.js
+++ src/connectors/twilioConnector.js
@@ -35,6 +35,28 @@ export default function TwilioConnector() {
           identity: userId,
           token: token.toJwt()
         };
+      },
+      createConferenceRoom: (parent, { title, users=[], messages=[] }, context, info) => {
+        const userId = context && context.userId;
+
+        if (!userId) {
+          throw new Error("Must be logged in, to create conference rooms");
+        }
+
+        return context.db.query.user({ where: { id: userId }})
+        .then((user) => {
+          if (!user) {
+            throw new Error("Authenticated user was not found");
+          }
+          return user
+        })
+        .then((user) => context.db.mutation.createConferenceRoom({
+          data: {
+            title,
+            messages: messages,
+            users: [user].concat(users),
+          }
+        }))
       }
     }
   };
